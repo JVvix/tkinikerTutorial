f293207eb7489c95bc96053b978ca6cc9fb1ae85
Faye Ly <fayedotly@gmail.com>
2020-06-19 17:20:30 -0700

    added label to cat image viewer

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -16,6 +16,8 @@ image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
+status = Label(root, text="Image 1 of " + str(len(image_list)))
+
 def forward(image_number):
     global my_label
     global button_forward
@@ -24,6 +26,7 @@ def forward(image_number):
     my_label = Label(image=image_list[image_number-1])
     button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
     button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)))
 
     if image_number == 5:
         button_forward = Button(root, text=">>", state=DISABLED)
@@ -31,6 +34,7 @@ def forward(image_number):
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
+    status.grid(row=2, column=0, columnspan=3)
 
 def back(image_number):
     global my_label
@@ -40,6 +44,7 @@ def back(image_number):
     my_label = Label(image=image_list[image_number-1])
     button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
     button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)))
 
     if image_number == 1:
         button_back = Button(root, text="<<", state=DISABLED)
@@ -47,6 +52,7 @@ def back(image_number):
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
+    status.grid(row=2, column=0, columnspan=3)
 
 button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_quit = Button(root, text="Exit Program", command=root.quit)
@@ -55,5 +61,6 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 button_back.grid(row=1, column=0)
 button_quit.grid(row=1, column=1)
 button_forward.grid(row=1, column=2)
+status.grid(row=2, column=0, columnspan=3)
 
 root.mainloop()
