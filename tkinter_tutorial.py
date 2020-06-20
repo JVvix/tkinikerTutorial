4865def56de43a6fe65d15c750980ff575d1e978
Faye Ly <fayedotly@gmail.com>
2020-06-19 18:41:19 -0700

    .

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -2,65 +2,16 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()
-root.title("Cat Image Viewer")
+root.title("Add Frames to Your Program")
 root.iconbitmap('icon.ico')
 
-my_img1 = ImageTk.PhotoImage(Image.open("download.jpg"))
-my_img2 = ImageTk.PhotoImage(Image.open("images3.jpg"))
-my_img3 = ImageTk.PhotoImage(Image.open("images.jpg"))
-my_img4 = ImageTk.PhotoImage(Image.open("images2.jpg"))
-my_img5 = ImageTk.PhotoImage(Image.open("download2.jpg"))
+frame = LabelFrame(root, padx=50, pady=50)
+frame.pack(padx=10, pady=10)
 
-image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+b = Button(frame, text="Don't Click This!")
+b.grid(row=0, column=0)
 
-my_label = Label(image=my_img1)
-my_label.grid(row=0, column=0, columnspan=3)
-
-status = Label(root, text="Image 1 of " + str(len(image_list)))
-
-def forward(image_number):
-    global my_label
-    global button_forward
-    global button_back
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
-    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)))
-
-    if image_number == 5:
-        button_forward = Button(root, text=">>", state=DISABLED)
-
-    my_label.grid(row=0, column=0, columnspan=3)
-    button_back.grid(row=1, column=0)
-    button_forward.grid(row=1, column=2)
-    status.grid(row=2, column=0, columnspan=3)
-
-def back(image_number):
-    global my_label
-    global button_forward
-    global button_back
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
-    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)))
-
-    if image_number == 1:
-        button_back = Button(root, text="<<", state=DISABLED)
-
-    my_label.grid(row=0, column=0, columnspan=3)
-    button_back.grid(row=1, column=0)
-    button_forward.grid(row=1, column=2)
-    status.grid(row=2, column=0, columnspan=3)
-
-button_back = Button(root, text="<<", command=back, state=DISABLED)
-button_quit = Button(root, text="Exit Program", command=root.quit)
-button_forward = Button(root, text=">>", command=lambda: forward(2))
-
-button_back.grid(row=1, column=0)
-button_quit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
-status.grid(row=2, column=0, columnspan=3)
+b2 = Button(frame, text="or here!")
+b2.grid(row=2, column=0)
 
 root.mainloop()
