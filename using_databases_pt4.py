58c79291901556cc9726633139f0fc780048d60c
JVvix <fayedotly@gmail.com>
2020-06-26 10:59:55 -0700

    finished final database tutorial

--- using_databases_pt4.py
+++ using_databases_pt4.py
@@ -21,6 +21,102 @@ c = conn.cursor()
 #         zipcode integer
 #        )""")
 
+def update():
+    conn = sqlite3.connect('address_book.db')
+    c = conn.cursor()
+    record_id = delete_box.get()
+
+    c.execute("""UPDATE addresses SET
+    first_name = :first,
+    last_name = :last,
+    address = :address,
+    city = :city,
+    state = :state,
+    zipcode = :zipcode
+
+    WHERE oid = :oid""",
+    {'first': f_name_editor.get(),
+    'last': l_name_editor.get(),
+    'address': address_editor.get(),
+    'city': city_editor.get(),
+    'state': state_editor.get(),
+    'zipcode': zipcode_editor.get(),
+
+    'oid': record_id
+
+    })
+
+    conn.commit()
+
+    conn.close()
+
+    editor.destroy()
+
+    f_name_editor.delete(0, END)
+    l_name_editor.delete(0, END)
+    address_editor.delete(0, END)
+    city_editor.delete(0, END)
+    state_editor.delete(0, END)
+    zipcode_editor.delete(0, END)
+
+def edit():
+    global editor
+    editor = Tk()
+    editor.title("Update a Record")
+    editor.geometry("400x400")
+
+    conn = sqlite3.connect('address_book.db')
+    c = conn.cursor()
+    
+    record_id = delete_box.get()
+
+    c.execute("SELECT * FROM addresses WHERE oid = " + record_id)
+    all_records = c.fetchall()
+
+    global f_name_editor 
+    global l_name_editor 
+    global address_editor 
+    global city_editor 
+    global state_editor 
+    global zipcode_editor 
+
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0, column=1, padx=20, pady=(10, 0))
+    l_name_editor = Entry(editor, width=30)
+    l_name_editor.grid(row=1, column=1)
+    address_editor = Entry(editor, width=30)
+    address_editor.grid(row=2, column=1)
+    city_editor = Entry(editor, width=30)
+    city_editor.grid(row=3, column=1)
+    state_editor = Entry(editor, width=30)
+    state_editor.grid(row=4, column=1)
+    zipcode_editor = Entry(editor, width=30)
+    zipcode_editor.grid(row=5, column=1)
+
+    f_name_label = Label(editor, text="First Name") 
+    f_name_label.grid(row=0, column=0, pady=(10, 0)) 
+    l_name_label = Label(editor, text="Last Name") 
+    l_name_label.grid(row=1, column=0)
+    address_label = Label(editor, text="Address") 
+    address_label.grid(row=2, column=0)
+    city_label = Label(editor, text="City") 
+    city_label.grid(row=3, column=0)
+    state_label = Label(editor, text="State") 
+    state_label.grid(row=4, column=0) 
+    zipcode_label = Label(editor, text="Zipcode") 
+    zipcode_label.grid(row=5, column=0)
+
+    for record in all_records:
+        f_name_editor.insert(0, record[0])
+        l_name_editor.insert(0, record[1])
+        address_editor.insert(0, record[2])
+        city_editor.insert(0, record[3])
+        state_editor.insert(0, record[4])
+        zipcode_editor.insert(0, record[5])
+
+    save_btn = Button(editor, text="Save Record", command=update)
+    save_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
 def delete():
     conn = sqlite3.connect('address_book.db')
 
@@ -123,7 +219,10 @@ query_btn = Button(root, text="Show Records", command=query)
 query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
 
 delete_btn = Button(root, text="Select Record", command=delete)
-delete_btn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=136)
+delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=136)
+
+delete_btn = Button(root, text="Edit Record", command=edit)
+delete_btn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=143)
 
 conn.commit()
 
